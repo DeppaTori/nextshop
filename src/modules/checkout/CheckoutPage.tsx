c436eff152546b1a3ee687a12befc59ece4e8a17
Tommy Toban <tommy.deppatori@gmail.com>
2022-08-01 21:30:10 +0700

    stlying checkout page

--- src/modules/checkout/CheckoutPage.tsx
+++ src/modules/checkout/CheckoutPage.tsx
@@ -1,6 +1,18 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useState, useEffect } from "react";
 import { Receiver, ShippingInfo } from "../../entity/ShippingInfo";
 import { API_URL } from "../../helpers/constant";
+import "./CheckoutPage.css";
 
 export const CheckoutPage = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -33,15 +45,40 @@ export const CheckoutPage = () => {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div>
-          <p>Informasi Pengiriman</p>
-          <p>Nama Penerima : {shippingInfo?.receiver.name}</p>
-          <p>Alamat Penerima : {shippingInfo?.receiver.address}</p>
-          <p>Metode Pembayaran : ATM Transfer</p>
-          <div>
-            <button>Payment</button>
-            <button>Continue Shopping</button>
-          </div>
+        <div className="Checkout-container">
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                Informasi Pengiriman
+              </Typography>
+              <List>
+                <ListItem>
+                  <ListItemText
+                    primary="Nama Penerima"
+                    secondary={shippingInfo?.receiver.name}
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemText
+                    primary="Alamat Penerima"
+                    secondary={shippingInfo?.receiver.address}
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemText
+                    primary="Metode Pembayaran"
+                    secondary="ATM Transfer"
+                  />
+                </ListItem>
+              </List>
+            </CardContent>
+            <CardActions>
+              <Button variant="outlined">Continue Shopping</Button>
+              <Button variant="contained">Payment</Button>
+            </CardActions>
+          </Card>
         </div>
       )}
     </>
